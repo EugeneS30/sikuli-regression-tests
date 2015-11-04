9b71c090f3329300cf339d8e6e96745b658fa7c8
eugene.shragovich <shragovich@gmail.com>
2015-11-04 15:48:04 +0800

    moved file

---
+++ src/test/java/com/eugenes/functional/hooks/Hook.java
@@ -0,0 +1,30 @@
+package com.eugenes.functional.hooks;
+
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.eugenes.functional.config.SikuliConfiguration;
+import com.eugenes.functional.config.WebDriverConfiguration;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+@ContextConfiguration(classes = {WebDriverConfiguration.class, SikuliConfiguration.class})
+public class Hook {
+
+    @Autowired
+    private WebDriver firefoxDriver;
+
+    @Before
+    public void setUp() {
+        firefoxDriver.get("http://www.sikulix.com");
+    }
+
+    @After
+    public void tearDown() {
+        // driver.close();
+
+    }
+
+}
