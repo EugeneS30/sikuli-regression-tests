78534ced391f91f0ebda2c858b36b2e5532561b5
eugene.shragovich <shragovich@gmail.com>
2015-11-06 15:26:11 +0800

    fixed proper screen bean intialization, added initialization settings

--- src/test/java/com/eugenes/functional/config/SikuliConfiguration.java
+++ src/test/java/com/eugenes/functional/config/SikuliConfiguration.java
@@ -1,19 +1,39 @@
 package com.eugenes.functional.config;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.sikuli.script.Screen;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import javax.annotation.PostConstruct;
 
-@ComponentScan
+import org.sikuli.basics.Settings;
+import org.sikuli.script.IScreen;
+import org.sikuli.script.ImagePath;
+import org.sikuli.script.Screen;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
 @Configuration
 public class SikuliConfiguration {
-    
+
+    @Value("${sikuli.bundlePath:classpath:src/test/resources/patterns}")
+    private String sikuliBundlePath;
+
+    @Value("${sikuli.autoWaitTimeout:3}")
+    private float autoWaitTimeout;
+
     @Bean
-    public Screen screen() {
+    public IScreen screen() {
         return new Screen();
+    }
+
+    @PostConstruct
+    public void setSikuliSettings() {
+        Settings.AutoWaitTimeout = autoWaitTimeout;
+        ImagePath.setBundlePath("C:/workspace/sikuli-regression-tests/src/test/resources/patterns/");
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 
 }
