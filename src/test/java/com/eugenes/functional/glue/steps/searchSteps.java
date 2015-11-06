30e643cd0de96b8abcc1c04ed20714ba609c1847
eugene.shragovich <shragovich@gmail.com>
2015-11-06 15:27:25 +0800

    moved file

---
+++ src/test/java/com/eugenes/functional/glue/steps/searchSteps.java
@@ -0,0 +1,54 @@
+package com.eugenes.functional.glue.steps;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.openqa.selenium.WebDriver;
+import org.sikuli.script.Region;
+import org.sikuli.script.Screen;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.eugenes.functional.config.SikuliConfiguration;
+import com.eugenes.functional.config.WebDriverConfiguration;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+@Slf4j
+@ContextConfiguration(classes = {WebDriverConfiguration.class, SikuliConfiguration.class})
+public class searchSteps {
+
+    @Autowired
+    private WebDriver driver;
+
+    @Autowired
+    private Screen screen;
+
+    @When("^I wait for element \"([^\"]*)\"$")
+    public void i_wait_for_element(final String patternName) throws Throwable {
+//        Region reg = screen.find(patternName);
+        log.info("Waiting for element: " + patternName + ". . .");
+        
+        screen.wait(patternName);
+
+    }
+
+    @Then("^the element is located$")
+    public void the_element_is_located() throws Throwable {
+
+    }
+
+    @When("^I search for element \"([^\"]*)\"$")
+    public void i_search_for_element() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I observe the screen for element \"([^\"]*)\"$")
+    public void i_observe_the_screen_for_element() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
