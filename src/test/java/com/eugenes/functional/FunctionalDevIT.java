e426722d2920b345a8de95376854f3cbb5e0ffe2
EugeneS <ez002622@MSF0795991157F.int.det.wa.edu.au>
2016-05-03 15:12:44 +0800

    comments

--- src/test/java/com/eugenes/functional/FunctionalDevIT.java
+++ src/test/java/com/eugenes/functional/FunctionalDevIT.java
@@ -7,25 +7,14 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 /**
- * This is useful for running in Eclipse to focus on scenarios in development rather than running
- * the whole test suite.
- *
- * Only scenarios tagged with @wip will be executed.
- *
+ * Manual runner.
+ * Will only execute the scenarios that are tagged with @tags 
+ * 
+ * @author eugene.shragovich
  */
 
 @RunWith(Cucumber.class)
-//@ContextConfiguration(classes=BeansClassConfig.class)
-@CucumberOptions(features = {"classpath:sikuli_features"})
-//@CucumberOptions(dryRun = false, 
-//                 strict = true, 
-//                 features = {"classpath:sikulix_features"}, 
-//                 format = {"json:target/cucumber-dev/cucumber.json", "pretty"}, 
-//                 monochrome = true, 
-//                 glue = {
-//                        "com.eugenes.functional"
-//                        }
-//                )
+@CucumberOptions(features = {"classpath:sikuli_features"}) //, tags = {"@wip"})
 
 
 public class FunctionalDevIT {
