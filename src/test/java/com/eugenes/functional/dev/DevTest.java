1c5bb1d58e31a1a0a50b014f3e157c1b9215be7d
EugeneS <ez002622@MSF0795991157F.int.det.wa.edu.au>
2016-05-03 15:46:53 +0800

    added

---
+++ src/test/java/com/eugenes/functional/dev/DevTest.java
@@ -0,0 +1,38 @@
+package com.eugenes.functional.dev;
+
+import java.util.Iterator;
+
+import org.junit.Test;
+import org.sikuli.basics.Settings;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.ImagePath;
+import org.sikuli.script.Match;
+import org.sikuli.script.Screen;
+
+public class DevTest {
+
+    @Test
+    public void highlightTest() throws FindFailed {
+
+        ImagePath.setBundlePath("C:/temp/");
+        Settings.MinSimilarity = 0.50;
+        String startBtn = "win7StartBtn.png";
+        String calcIcon = "calcIconSmall.png";
+        String football = "footballImg.png";
+
+        Screen s = new Screen(1);
+        // Match m = s.find(calcIcon);
+        // m.highlight(3);
+        // Match m1 = s.wait(imageName);
+
+        for (Iterator<Match> list = s.findAll(football); list.hasNext();) {
+            Match m = list.next();
+            m.highlight(5);
+        }
+
+        // for (Match m : s.findAll(football)) {
+        // m.highlight(1);
+        // }
+
+    }
+}
