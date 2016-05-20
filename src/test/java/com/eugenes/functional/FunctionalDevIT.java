041905ea49eb7c2c22a336cf62a2640bcef4ce0d
EugeneS <ez002622@MSF0795991157F.int.det.wa.edu.au>
2016-05-20 16:10:10 +0800

    added report path

--- src/test/java/com/eugenes/functional/FunctionalDevIT.java
+++ src/test/java/com/eugenes/functional/FunctionalDevIT.java
@@ -7,17 +7,15 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 /**
- * Manual runner.
- * Will only execute the scenarios that are tagged with @tags 
+ * Manual runner. Will only execute the scenarios that are tagged with @tags
  * 
  * @author eugene.shragovich
  */
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"classpath:sikuli_features"}) //, tags = {"@wip"})
-
-
-public class FunctionalDevIT {
-    
+@CucumberOptions(features = {"classpath:sikuli_features"}, format = {"json:target/cucumber-dev/cucumber.json", "pretty"})
+// , tags = {"@wip"})
+public class FunctionalDevIT
+{
 
 }
