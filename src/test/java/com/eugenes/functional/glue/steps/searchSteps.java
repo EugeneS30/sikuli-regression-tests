3ed1b2294dcf56fccdb08162c39c576c904faa48
EugeneS <ez002622@MSF0795991157F.int.det.wa.edu.au>
2016-06-02 10:38:45 +0800

    i_observe_the_screen_for_element step amended

--- src/test/java/com/eugenes/functional/glue/steps/searchSteps.java
+++ src/test/java/com/eugenes/functional/glue/steps/searchSteps.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.sikuli.script.FindFailed;
 import org.sikuli.script.Match;
 import org.sikuli.script.Pattern;
+import org.sikuli.script.Region;
 import org.sikuli.script.Screen;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -21,21 +22,21 @@ import cucumber.api.java.en.When;
 @Slf4j
 @Component
 public class searchSteps extends AbstractSteps {
-    
+
     @Autowired
     private Screen screen;
-    
+
     @Setter
     private Match storedMatch;
-    
+
     @Setter
     private Pattern storedPattern;
 
     @When("^I wait for pattern \"([^\"]*)\"$")
     public void i_wait_for_pattern(final String patternName) throws FindFailed {
-        
+
         log.info("Waiting for pattern: " + patternName + ". . .");
-        
+
         storedPattern = new Pattern(patternName);
         setStoredMatch(screen.wait(patternName));
 
@@ -43,20 +44,22 @@ public class searchSteps extends AbstractSteps {
 
     @When("^I observe the screen for pattern \"([^\"]*)\"$")
     public void i_observe_the_screen_for_element(final String patternName) throws Throwable {
-        
+
         log.info("Observing for pattern: " + patternName + ". . .");
-        
-        assertThat(screen.observe()).isTrue();
-        
+
+        Region region = screen;
+        region.onAppear(patternName);
+
+        assertThat(region.observe(100)).isTrue();
+
     }
-    
+
     @Then("^the pattern \"([^\"]*)\" exists$")
     public void the_element_exists(final String patternName) throws Throwable {
-        
+
         assertThat(screen.exists(storedPattern)).isNotNull();
         assertThat(storedMatch.getScore()).isGreaterThan(0.95);
         storedMatch = null;
 
     }
-
 }
