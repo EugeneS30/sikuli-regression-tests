63bf5cc207e3a90e59ecffb202a8c6603a3f60eb
EugeneS <shragovich@gmail.com>
2016-06-10 16:02:27 +0800

    created dry runner

---
+++ src/test/java/com/eugenes/functional/FunctionalDryRunIT.java
@@ -0,0 +1,20 @@
+package com.eugenes.functional;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+/**
+ * Manual runner. Will only execute the scenarios that are tagged with @tags
+ * 
+ * @author eugene.shragovich
+ */
+
+@RunWith(Cucumber.class)
+@CucumberOptions(dryRun = true, features = {"classpath:sikuli_features"}, format = {"json:target/cucumber-dev/cucumber.json", "pretty"})
+
+public class FunctionalDryRunIT
+{
+
+}
