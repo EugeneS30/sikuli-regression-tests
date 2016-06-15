1c777abaa0a4f364dc84088cbcb84e77e79b4102
EugeneS <shragovich@gmail.com>
2016-06-15 11:10:28 +0800

    commit the changes

--- src/test/java/com/eugenes/functional/glue/steps/navigationSteps.java
+++ src/test/java/com/eugenes/functional/glue/steps/navigationSteps.java
@@ -7,24 +7,45 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 
 public class navigationSteps extends AbstractSteps {
-    
-    @Autowired
-    private WebDriver driver;
 
-    @When("^I navigate to \"(.*?)\"$")
-    @Given("^I have navigated to \"(.*?)\"$")
-    public void i_have_navigated_to(String url) throws Throwable {
-        
-        driver.get(url);
-        
-    }
-    
-    @When("^navigate to a blank page$")
-    public void navigate_to_blank_page() throws Throwable {
-    	
-    	i_have_navigated_to("about:blank");
+	@Autowired
+	private WebDriver driver;
 
-    }
+	@When("^I navigate to \"(.*?)\"$")
+	@Given("^I have navigated to \"(.*?)\"$")
+	public void i_have_navigated_to(String url) throws Throwable {
 
+		driver.get(url);
+
+	}
+
+	/**
+	 * This step is to be used for navigation where we do not want to wait until
+	 * the page is fully loaded.
+	 */
+	@When("^I am navigating to \"(.*?)\"$")
+	public void i_am_navigating_to(String url) throws Throwable {
+
+		Thread t = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+
+				driver.get(url);
+
+			}
+		});
+
+		t.start();
+
+	}
+
+	@Given("^I have navigated to a blank page$")
+	@When("^navigate to a blank page$")
+	public void navigate_to_blank_page() throws Throwable {
+
+		i_have_navigated_to("about:blank");
+
+	}
 
 }
--- src/test/java/com/eugenes/functional/glue/steps/searchSteps.java
+++ src/test/java/com/eugenes/functional/glue/steps/searchSteps.java
@@ -4,7 +4,6 @@ import static org.fest.assertions.Assertions.assertThat;
 
 import javax.inject.Inject;
 
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import org.sikuli.script.FindFailed;
@@ -31,106 +30,123 @@ import cucumber.api.java.en.When;
 @Component
 public class searchSteps extends AbstractSteps {
 
-    @Autowired
-    private Screen screen;
-    
-    @Inject
-    private SikuliSupport sikuli;
+	@Autowired
+	private Screen screen;
 
-    // If the pattern was found by one of the steps it will be stored in this variable.
-    private Match storedMatch;
+	@Inject
+	private SikuliSupport sikuli;
 
-    public void setStoredMatch(Match storedMatch) {
+	// If the pattern was found by one of the steps it will be stored in this
+	// variable.
+	private Match storedMatch;
+
+	public void setStoredMatch(Match storedMatch) {
 		this.storedMatch = storedMatch;
 	}
 
-    private Pattern storedPattern;
-    
-    public void setStoredPattern(Pattern storedPattern) {
+	private Pattern storedPattern;
+
+	public void setStoredPattern(Pattern storedPattern) {
 		this.storedPattern = storedPattern;
 	}
 
 	private boolean observationOutcome;
-    
-    public void setObservationOutcome(boolean observationOutcome) {
+
+	public void setObservationOutcome(boolean observationOutcome) {
 		this.observationOutcome = observationOutcome;
 	}
 
 	@Value("${sikuli.observeTimeout.time:60}")
-    private float observeTimeout;
+	private float observeTimeout;
 
-    @When("^I wait for pattern \"([^\"]*)\"$")
-    public void i_wait_for_pattern(final String patternName) throws FindFailed {
+	@Value("${sikuli.waitTimeout.time:60}")
+	private float waitTimeout;
 
-        log.info("Waiting for pattern: " + patternName + ". . .");
+	@When("^I wait for pattern \"([^\"]*)\"$")
+	public void i_wait_for_pattern(final String patternName) throws FindFailed {
 
-        setStoredPattern(new Pattern(patternName));
-        setStoredMatch(screen.wait(patternName));
+		log.info("Waiting for pattern: " + patternName + ". . .");
 
-    }
+		setStoredPattern(new Pattern(patternName));
+		setStoredMatch(screen.wait(patternName, waitTimeout));
 
-    @When("^I observe the screen for pattern \"(.*?)\" to \"(.*?)\"$")
-    public void i_observe_the_screen_for_pattern_to(final String pattern, final String eventType) throws Throwable {
+	}
 
-        log.info("Setting initial outcome to False");     
-        setObservationOutcome(false);
+	@When("^I observe the screen for pattern \"(.*?)\" to \"(.*?)\"$")
+	public void i_observe_the_screen_for_pattern_to(final String pattern,
+			final String eventType) throws Throwable {
 
-        Region region = screen;
+		log.info("Setting initial outcome to False");
+		setObservationOutcome(false);
 
-        switch (ObserverEvent.fromString(eventType)) {
-            case ON_APPEAR:
-            	log.info("ON_APPEAR");
-                region.onAppear(pattern);
-                break;
+		Region region = screen;
 
-            case ON_VANISH:
-            	log.info("ON_VANISH");
-                region.onVanish(pattern);
-                break;
+		switch (ObserverEvent.fromString(eventType)) {
+		case ON_APPEAR:
+			log.info("ON_APPEAR");
+			region.onAppear(pattern);
+			break;
 
-            case ON_CHANGE:
-            	log.info("ON_CHANGE");
-                region.onChange();
+		case ON_VANISH:
+			log.info("ON_VANISH");
+			region.onVanish(pattern);
+			break;
 
-            default:
-                throw new PendingException("event type not supported: " + eventType);
-        }
-        
-        log.info("Starting the observation");
-        boolean outcome = region.observeInBackground(observeTimeout);
-        log.info("Observation completed with result: {}! Setting the outcome accordingly", outcome);
-        setObservationOutcome(outcome);
-        
-    }
+		case ON_CHANGE:
+			log.info("ON_CHANGE");
+			region.onChange();
 
-    @Then("^the event fires$")
-    public void the_onAppear_event_fires() throws Throwable {
-        
-        assertThat(observationOutcome).isTrue();
+		default:
+			throw new PendingException("event type not supported: " + eventType);
+		}
 
-//        setStoredPattern(new Pattern(pattern));
-//        setStoredMatch(screen.find(pattern));
- 
-    }
+		log.info("Starting the observation");
+		boolean outcome = region.observeInBackground(observeTimeout);
+		log.info(
+				"Observation completed with result: {}! Setting the outcome accordingly",
+				outcome);
+		setObservationOutcome(outcome);
 
-    @Given("^the pattern \"(.*?)\" (does|does not) exist on the screen$")
-    public void the_pattern_maybe_visible_on_the_screen(final String pattern, final String maybe) throws Throwable {
+	}
 
-        boolean isShown = "does".equals(maybe);
+	@Then("^the event fires$")
+	public void the_onAppear_event_fires() throws Throwable {
 
-        assertThat(sikuli.doesExist(pattern)).isEqualTo(isShown);
+		assertThat(observationOutcome).isTrue();
 
-    }
+		// setStoredPattern(new Pattern(pattern));
+		// setStoredMatch(screen.find(pattern));
 
-    @Then("^the pattern \"([^\"]*)\" exists$")
-    public void the_element_exists(final String patternName) throws Throwable {
+	}
 
-        log.info("Checking that not null");
-        assertThat(screen.exists(storedPattern)).isNotNull();
-        log.info("Checked");
-        // assertThat(storedMatch.getScore()).isGreaterThan(0.95);
-        assertThat(storedMatch.getScore()).isGreaterThan(0.9);
-        storedMatch = null;
+	@Given("^the pattern \"(.*?)\" (exists|does not exist) on the screen$")
+	public void the_pattern_maybe_visible_on_the_screen(final String pattern,
+			final String maybe) throws Throwable {
 
-    }
+		boolean isShown = "exists".equals(maybe);
+		
+		if (isShown) {
+			assertThat(sikuli.doesExist(pattern)).isTrue();
+			assertThat(storedMatch.getScore()).isGreaterThan(0.9);
+			storedMatch = null;
+			storedPattern = null;
+		} else {
+			assertThat(sikuli.doesExist(pattern)).isFalse();
+		}
+
+		
+
+	}
+
+//	@Then("^the pattern \"([^\"]*)\" exists$")
+//	public void the_element_exists(final String patternName) throws Throwable {
+//
+//		log.info("Checking that not null");
+//		assertThat(screen.exists(storedPattern)).isNotNull();
+//		log.info("Checked");
+//		// assertThat(storedMatch.getScore()).isGreaterThan(0.95);
+//		assertThat(storedMatch.getScore()).isGreaterThan(0.9);
+//		storedMatch = null;
+//
+//	}
 }
