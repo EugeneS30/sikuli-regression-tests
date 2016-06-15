22ad22767371fbbd6c88cefc8786cfdc664a9791
EugeneS <shragovich@gmail.com>
2016-06-15 17:26:53 +0800

    added comments

--- src/test/java/com/eugenes/functional/util/SikuliSupport.java
+++ src/test/java/com/eugenes/functional/util/SikuliSupport.java
@@ -13,18 +13,24 @@ import org.springframework.stereotype.Component;
  */
 
 public class SikuliSupport {
-    
+
     @Inject
     private Screen screen;
-    
+
+    /**
+     * This method is to be used when a boolean needed instead of a Match object that exists()
+     * normally returns.
+     * 
+     * @return boolean
+     */
     public boolean doesExist(final String pattern) {
-        
+
         if (screen.exists(pattern) != null) {
             return true;
         }
-        
+
         return false;
-        
+
     }
 
 }
